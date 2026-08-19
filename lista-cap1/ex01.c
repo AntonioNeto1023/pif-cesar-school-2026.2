998e2cadeb2f6d072db3fc90bbb3f6d02d608af3
Antonio Neto <neto90206@gmail.com>
2026-08-19 13:54:16 -0300

    Add files via upload

---
+++ lista-cap1/ex01.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+    int idade = 18;
+    printf("Voce tem %d anos", idade);
+    return 0;
+}
---
+++ lista-cap1/ex02.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+    float e = 2.72828;
+    printf("O valor da constante de euler e %.3f", e);
+    
+    return 0;
+}
